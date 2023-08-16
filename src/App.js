2d7293b80b4186ab16044ddd515af7aafa371b44
pavankumarvangala <45650416+pavankumarvangala@users.noreply.github.com>
2023-08-16 19:09:58 +0530

    Router configuration added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Home from "./components/Home";
+import ProductDetails from "./components/ProductDetails";
+import ProductList from "./components/ProductList";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <h4>Welcome to M-Zone</h4>
+      <RouterProvider router={appRouter} />
+      <Outlet />
     </div>
   );
 }
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/productDetails",
+    element: <ProductDetails />,
+  },
+  {
+    path: "/productList",
+    element: <ProductList />,
+  },
+]);
 
 export default App;
---
+++ src/utils/constants.js
@@ -0,0 +1,80 @@
+export const productsList = {
+  electronics: [
+    {
+      product_id: "B07H65KP63",
+      product_name: "Amazon Echo Dot (3rd Gen)",
+      price: 29.99,
+      currency: "USD",
+      rating: 4.5,
+      availability: true,
+      seller: {
+        seller_name: "Amazon",
+        seller_rating: 4.8,
+      },
+    },
+    {
+      product_id: "B0742S4TXG",
+      product_name: "Apple iPhone 12 Pro",
+      price: 999.99,
+      currency: "USD",
+      rating: 4.9,
+      availability: true,
+      seller: {
+        seller_name: "Apple",
+        seller_rating: 4.7,
+      },
+    },
+  ],
+  clothing: [
+    {
+      product_id: "B08Q5C1S72",
+      product_name: "Levi's Men's 501 Original Fit Jeans",
+      price: 49.99,
+      currency: "USD",
+      rating: 4.7,
+      availability: true,
+      seller: {
+        seller_name: "Levi's",
+        seller_rating: 4.6,
+      },
+    },
+    {
+      product_id: "B07TJ7T2JD",
+      product_name: "Adidas Originals Superstar Sneakers",
+      price: 79.95,
+      currency: "USD",
+      rating: 4.8,
+      availability: true,
+      seller: {
+        seller_name: "Adidas",
+        seller_rating: 4.9,
+      },
+    },
+  ],
+  books: [
+    {
+      product_id: "0385472579",
+      product_name: "To Kill a Mockingbird",
+      price: 8.99,
+      currency: "USD",
+      rating: 4.8,
+      availability: true,
+      seller: {
+        seller_name: "HarperCollins",
+        seller_rating: 4.7,
+      },
+    },
+    {
+      product_id: "0061120081",
+      product_name: "The Great Gatsby",
+      price: 12.95,
+      currency: "USD",
+      rating: 4.6,
+      availability: true,
+      seller: {
+        seller_name: "Scribner",
+        seller_rating: 4.5,
+      },
+    },
+  ],
+};
